aa95fd827c51c6590dfe54888f0a7bd182a10f01
JarnoStudent <481737@student.fontys.nl>
2022-08-02 19:52:46 +0200

    Steam login
    Added the ability to login through Steam.

---
+++ Web/Logic/Service/SteamService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Logic.Service
+{
+    public static class SteamService
+    {
+        private static string baseUrl = "https://steamcommunity.com/";
+        private static HttpClient steamAuthenticateClient = new();
+        /// <summary>
+        /// Sends the user to steam. Steam will then authenticate the user throuhg their login system.
+        /// </summary>
+        /// <returns>Returns true if user was succesfully authenticated</returns>
+        public static async Task<string> GetLoginUrl(string hostUrl)
+        {
+            string loginUrl = baseUrl + "openid/login";
+
+            var query = HttpUtility.ParseQueryString(string.Empty);
+            query["openid.ns"] = "http://specs.openid.net/auth/2.0";
+            query["openid.mode"] = "checkid_setup";
+            query["openid.return_to"] = $"https://{hostUrl}";
+            query["openid.realm"] = $"https://{hostUrl}";
+            query["openid.identity"] = "http://specs.openid.net/auth/2.0/identifier_select";
+            query["openid.claimed_id"] = "http://specs.openid.net/auth/2.0/identifier_select";
+
+            string parameters = query.ToString();
+
+            loginUrl += $"?{parameters}";
+            
+            return loginUrl;
+        }
+    }
+}
---
+++ Web/Web/Controllers/SessionController.cs
@@ -0,0 +1,17 @@
+﻿using Logic.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class SessionController : Controller
+    {
+        public IActionResult Login()
+        {
+            string hostUrl = Request.Host.ToString();
+
+            string loginUrl = SteamService.GetLoginUrl(hostUrl).Result;
+
+            return Redirect(loginUrl);
+        }
+    }
+}
